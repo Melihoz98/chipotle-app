504c60284595d6302dcc38a4615363bde8d44f90
Melih Øzata <10487163@ucn.dk>
2024-04-14 11:25:00 +0200

    Home edit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import name from '../TekstNavn-removebg-preview(1).png'; // Import the logo image
-
+import logo from '../logomexico-removebg-preview.png'; // Import the logo image
+import './HomeStyle.css';
 const Home = () => {
   return (
     <div className="home">
       <img src={name} alt="Navn" style={{ width: '600px', height: 'auto', marginRight: '10px' }} /> 
+      <img src={logo} alt="Logo" style={{ width: '300px', height: 'auto', marginRight: '10px' }} /> 
+
+      <h2>Velkommen til vores mexicanske steakhouse!<br></br>
+        Smag på vores saftige bøffer med en krydret twist. <br></br>
+        Oplev ægte mexicansk gastronomi i hjertet af byen!</h2>
       <button 
         type="button" 
         className="btn custom-btn" 
@@ -43,6 +49,7 @@ const Home = () => {
       >
         Book bord
       </button>
+      
     </div>
   );
 };
